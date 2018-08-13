b929bc4746e0421c3687fa457d58571734aee17d
hpq <hepeiqin@casachina.com.cn>
2018-08-13 09:51:51 +0800

    implementation

---
+++ index.vue
@@ -0,0 +1,26 @@
+<template>
+    <svg class="icon" aria-hidden="true">
+        <use :xlink:href="iconName"></use>
+        <slot></slot>
+    </svg>
+</template>
+
+<script>
+export default {
+    name: 'icon',
+    computed: {
+        iconName: function() {
+            return "#icon-" +  this.$slots.default[0].text;
+        }
+    },
+}
+</script>
+
+<style scoped>
+.icon {
+    width: 0.8em; height: 0.8em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+ }
+</style>
